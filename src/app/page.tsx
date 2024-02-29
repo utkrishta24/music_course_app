3eeb8fc8bd3e51bb200add52582f198ae3df08d6
utkrishta24 <krishsinha24062gmail.com>
2024-02-29 18:34:14 +0530

    splash

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import Instructors from "@/components/Instructors";
 import TestimonialCards from "@/components/TestimonialCards";
 import UpcomingWebinars from "@/components/UpcomingWebinars";
 import WhyChooseUs from "@/components/WhyChooseUs";
+
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] atialiased bg-grid-white/[0.02]">
--- src/components/SplashScreen.tsx
+++ src/components/SplashScreen.tsx
@@ -1,10 +1,10 @@
 "use client";
 import React from "react";
-import { SparklesCore } from "../../components/ui/sparkles";
+import { SparklesCore } from "./ui/sparkles";
 
-function LoadingScreen() {
+export function SplashScreen() {
   return (
-    <div className="h-[40rem] w-full bg-black flex flex-col items-center justify-center overflow-hidden rounded-md">
+    <div className="h-[50rem] w-full bg-black flex flex-col items-center justify-center overflow-hidden rounded-md">
       <h1 className="md:text-7xl text-3xl lg:text-9xl font-bold text-center text-white relative z-20">
         Utkrishta Sinha
       </h1>
@@ -14,7 +14,7 @@ function LoadingScreen() {
         <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-px w-3/4" />
         <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-[5px] w-1/4 blur-sm" />
         <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-px w-1/4" />
- 
+
         {/* Core component */}
         <SparklesCore
           background="transparent"
@@ -24,12 +24,10 @@ function LoadingScreen() {
           className="w-full h-full"
           particleColor="#FFFFFF"
         />
- 
+
         {/* Radial Gradient to prevent sharp edges */}
         <div className="absolute inset-0 w-full h-full bg-black [mask-image:radial-gradient(350px_200px_at_top,transparent_20%,white)]"></div>
       </div>
     </div>
   );
 }
-
-export default LoadingScreen;
